ad2cec7ce1c483aedef3c042c05ea2ade9b84cde
junayed17 <junayedahmednipun@gmail.com>
2025-11-09 17:35:52 +0600

    basic setup done

---
+++ src/Components/Navber.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navber = () => {
+  return (
+    <div>
+      i am navber
+    </div>
+  );
+};
+
+export default Navber;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navber from '../Components/Navber';
+import Footer from '../Components/footer/Footer';
+import { Outlet } from 'react-router';
+
+const Home = () => {
+  return (
+    <div>
+      <header>
+        <Navber/>
+      </header>
+      <main>
+<Outlet/>
+      </main>
+      <footer>
+        <Footer/>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
