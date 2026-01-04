2352a6bf5749aee0cef95b1704b00adba794da63
junayed17 <junayedahmednipun@gmail.com>
2026-01-04 12:53:03 +0600

    fotter polished

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebook, FaTwitter, FaXTwitter, FaYoutube } from "react-icons/fa6";
 
 const Footer = () => {
   return (
@@ -7,10 +7,19 @@ const Footer = () => {
         <nav>
           <h6 className="footer-title font-bold text-lg mb-2">Contact Us</h6>
           <p className="link link-hover font-semibold">
-            Email: <a className="link link-hover">support@krisilink.com</a>
+            Email:
+            <a
+              href="mailto:junayedahmednipun@gmail.com"
+              className="link link-hover"
+            >
+              support@krisilink.com
+            </a>
           </p>
           <p className="link link-hover font-semibold">
-            Phone:<a className="link link-hover"> +880 123 456 789</a>
+            Phone:
+            <a href="tel:+8801822266547" className="link link-hover">
+              +880 1822266547
+            </a>
           </p>
           <p className="link link-hover font-semibold">
             <a className="link link-hover">
@@ -21,31 +30,32 @@ const Footer = () => {
 
         <nav>
           <h6 className="footer-title font-bold text-lg mb-2">Follow Us</h6>
-          <div className="grid grid-flow-col gap-4">
-            <a>
-              <FaXTwitter className="w-6 h-6"/>
+          <div className="grid grid-flow-col gap-6">
+            {/* Twitter (X) - Hover color: Black */}
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              className="text-gray-500 transition-all duration-300 hover:text-[#ffffff] hover:scale-110"
+            >
+              <FaXTwitter className="w-7 h-7" />
             </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-              </svg>
+
+            {/* YouTube - Hover color: Red */}
+            <a
+              href="https://www.youtube.com/@shykhseraj"
+              target="_blank"
+              className="text-gray-500 transition-all duration-300 hover:text-[#FF0000] hover:scale-110"
+            >
+              <FaYoutube className="w-7 h-7" />
             </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-              </svg>
+
+            {/* Facebook - Hover color: Blue */}
+            <a
+              href="https://www.facebook.com/shykhseraj"
+              target="_blank"
+              className="text-gray-500 transition-all duration-300 hover:text-[#1877F2] hover:scale-110"
+            >
+              <FaFacebook className="w-7 h-7" />
             </a>
           </div>
         </nav>
@@ -54,8 +64,8 @@ const Footer = () => {
           <h6 className="ffooter-title font-bold text-lg mb-2">Privacy</h6>
           <a className="link link-hover font-semibold">Privacy Policy</a>
           <p>
-            We use cookies to enhance your experience. By using our site, you
-            agree to our use of cookies.
+            We use Firebase services to improve your experience. By using our
+            site, you agree to our privacy policy.
           </p>
         </nav>
       </div>
