bb941f2af9b4fe2078ee049257cf7186c66341a6
junayed17 <junayedahmednipun@gmail.com>
2026-01-04 12:28:42 +0600

    slider polish with 70 % w

--- src/slider/Slider.jsx
+++ src/slider/Slider.jsx
@@ -7,10 +7,12 @@ import "swiper/css/autoplay";
 import "./styles.css";
 
 import { Navigation, Autoplay } from "swiper/modules";
+import { Link } from "react-router";
+import { FaArrowRight } from "react-icons/fa6";
 
 export default function App() {
   return (
-    <>
+    <section className="h-[70vh]">
       <Swiper
         navigation={true}
         modules={[Navigation, Autoplay]}
@@ -20,79 +22,96 @@ export default function App() {
           pauseOnMouseEnter: true,
         }}
         loop={true}
-        className="mySwiper max-w-[1600px] mx-auto mb-6"
+        className="mySwiper max-w-[1600px] mx-auto mb-6 h-full"
       >
-        <SwiperSlide>
-          <div className="relative">
+        <SwiperSlide className="h-full">
+          <div className="relative h-full text-center">
             <img
-              src="https://themewagon.github.io/AgriCulture/assets/img/hero_1.jpg"
+              // src="https://themewagon.github.io/AgriCulture/assets/img/hero_1.jpg"
+              src="https://media.istockphoto.com/id/506164764/photo/tractor-spraying-soybean-field.jpg?s=612x612&w=0&k=20&c=h27yHr07QNSghYS20iwYBCGjZIa2HlXqrZDkM0ZsYEw="
               alt="Slide 1"
-              className="w-full object-cover"
+              className="w-full object-cover h-full"
             />
-            <h1
+            <div
               className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
-  sliderFont text-[20px] sm:text-2xl md:text-4xl  lg:text-5xl text-[#fff200] drop-shadow-[0_3px_6px_rgba(0,0,0,0.8)] "
+  "
               // data-aos="fade-up"
             >
-              “Bridging Farmers, Traders & Consumers — Growing Together with
-              KrishiLink.”
-            </h1>
+              <h1 className="sliderFont text-[20px] sm:text-2xl md:text-4xl  lg:text-5xl text-[#fff200] drop-shadow-[0_3px_6px_rgba(0,0,0,0.8)] ">
+                “Connecting Farmers, Traders & Consumers.”
+              </h1>
+              <Link
+                to="/AllCrops"
+                className="inline-flex items-center justify-center gap-2  headingFont px-4 py-2 rounded-xl bg-green-500 hover:bg-green-600 hover:text-white font-bold transition-all z-20 text-center mt-2 shadow"
+              >
+                Explore posts
+              </Link>
+            </div>
           </div>
         </SwiperSlide>
 
-        <SwiperSlide>
-          <div className="relative">
+        <SwiperSlide className="h-full">
+          <div className="relative h-full text-center">
             <img
-              src="https://themewagon.github.io/AgriCulture/assets/img/hero_2.jpg"
+              // src="https://themewagon.github.io/AgriCulture/assets/img/hero_2.jpg"
+              src="https://img.freepik.com/free-photo/composition-fresh-vegetables-blurred-vegetable-garden-background_169016-40138.jpg?semt=ais_hybrid&w=740&q=80"
               alt="Slide 2"
-              className="w-full object-cover"
+              className="w-full object-cover h-full"
             />
-            <h1
-              className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
-  sliderFont text-[20px] sm:text-2xl md:text-4xl  lg:text-5xl text-[#fff200] drop-shadow-[0_3px_6px_rgba(0,0,0,0.8)] "
-              // data-aos="fade-up"
-            >
-              “From Fields to Future — KrishiLink connects every hand that feeds
-              the nation.”
-            </h1>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+              <h1 className="sliderFont text-[20px] sm:text-2xl md:text-4xl  lg:text-5xl text-[#fff200] drop-shadow-[0_3px_6px_rgba(0,0,0,0.8)]">
+                “From Fields to Future — Feeding the Nation Together.”
+              </h1>
+              <Link
+                to="/AllCrops"
+                className="inline-flex items-center justify-center gap-2  headingFont px-4 py-2 rounded-xl bg-green-500 hover:bg-green-600 hover:text-white font-bold transition-all z-20 text-center mt-2 shadow"
+              >
+                Explore posts
+              </Link>
+            </div>
           </div>
         </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="relative">
+        <SwiperSlide className="h-full">
+          <div className="relative h-full text-center">
             <img
               src="https://themewagon.github.io/AgriCulture/assets/img/hero_3.jpg"
               alt="Slide 3"
-              className="w-full object-cover"
+              className="w-full object-cover h-full"
             />
-            <h1
-              className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
-  sliderFont text-[20px] sm:text-2xl md:text-4xl  lg:text-5xl text-[#fff200] drop-shadow-[0_3px_6px_rgba(0,0,0,0.8)] "
-              // data-aos="fade-up"
-            >
-              “Not just a marketplace — a social agro network for growth and
-              collaboration.”
-            </h1>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+              <h1 className="sliderFont text-[20px] sm:text-2xl md:text-4xl  lg:text-5xl text-[#fff200] drop-shadow-[0_3px_6px_rgba(0,0,0,0.8)]">
+                “Beyond Marketplace. Built for Agro Growth.”
+              </h1>
+              <Link
+                to="/AllCrops"
+                className="inline-flex items-center justify-center gap-2  headingFont px-4 py-2 rounded-xl bg-green-500 hover:bg-green-600 hover:text-white font-bold transition-all z-20 text-center mt-2 shadow"
+              >
+                Explore posts
+              </Link>
+            </div>
           </div>
         </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="relative">
+        <SwiperSlide className="h-full">
+          <div className="relative h-full text-center">
             <img
               src="https://themewagon.github.io/AgriCulture/assets/img/hero_4.jpg"
               alt="Slide 4"
-              className="w-full  object-cover"
+              className="w-full  object-cover h-full"
             />
-            <h1
-              className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
-  sliderFont text-[20px] sm:text-2xl md:text-4xl  lg:text-5xl text-[#fff200] drop-shadow-[0_3px_6px_rgba(0,0,0,0.8)] "
-            >
-              “Empowering Farmers, Uniting Communities — The Green Connection
-              Begins Here.”
-            </h1>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+              <h1 className="sliderFont text-[20px] sm:text-2xl md:text-4xl  lg:text-5xl text-[#fff200] drop-shadow-[0_3px_6px_rgba(0,0,0,0.8)]">
+                “Empowering Farmers & Communities — The Green Connection.”
+              </h1>
+              <Link
+                to="/AllCrops"
+                className="inline-flex items-center justify-center gap-2  headingFont px-4 py-2 rounded-xl bg-green-500 hover:bg-green-600 hover:text-white font-bold transition-all z-20 text-center mt-2 shadow"
+              >
+                Explore posts
+              </Link>
+            </div>
           </div>
         </SwiperSlide>
       </Swiper>
-    </>
+    </section>
   );
 }
